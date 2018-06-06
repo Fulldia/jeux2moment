87a40e58331b8874a93617e896d10765ed424d15
Camille Lafourcade <camille.lafourcade64@orange.fr>
2018-06-06 17:28:37 +0200

    jeuxdumoment

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606121336) do
+ActiveRecord::Schema.define(version: 20180606145423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.string "string"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subscribers", force: :cascade do |t|
     t.string "name"
